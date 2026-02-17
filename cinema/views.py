53853fdba3a3fd931ec5a95d4f66d390239bd933
Maksym Pakula <gargantadoto@gmail.com>
2026-02-17 05:09:28 +0200

    Fix mistake with getting non-existing genre

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import viewsets, mixins, generics, status
@@ -31,7 +32,7 @@ class GenreList(APIView):
 
 class GenreDetail(APIView):
     def get_object(self, pk):
-        genre = Genre.objects.get(pk=pk)
+        genre = get_object_or_404(Genre, pk=pk)
         return genre
 
     def get(self, request, pk):
