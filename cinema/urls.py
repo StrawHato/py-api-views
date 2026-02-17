631d8c15546c76dcb0c968c0b48b17501d2e48da
Maksym Pakula <gargantadoto@gmail.com>
2026-02-17 04:48:34 +0200

    Add views for Genre, Actor, CinemaHall, Movie models and corresponding endpoints

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,10 +1,31 @@
 from django.urls import path
+from rest_framework import routers
+from cinema.views import (
+    MovieViewSet,
+    GenreList,
+    GenreDetail,
+    ActorList,
+    ActorDetail,
+    CinemaHallList,
+    CinemaHallDetail,
+)
 
-from cinema.views import movie_list, movie_detail
+router = routers.DefaultRouter()
+router.register("movies", MovieViewSet, basename="movies")
 
 urlpatterns = [
-    path("movies/", movie_list, name="movie-list"),
-    path("movies/<int:pk>/", movie_detail, name="movie-detail"),
+    path("genres/", GenreList.as_view(), name="genres-list"),
+    path("genres/<int:pk>/", GenreDetail.as_view(), name="genres-detail"),
+    path("actors/", ActorList.as_view(), name="actors-list"),
+    path("actors/<int:pk>/", ActorDetail.as_view(), name="actors-detail"),
+    path("cinema-hall/", CinemaHallList.as_view(), name="cinema-hall-list"),
+    path(
+        "cinema-hall/<int:pk>/",
+        CinemaHallDetail.as_view(),
+        name="cinema-hall-detail"
+    ),
 ]
+
+urlpatterns += router.urls
 
 app_name = "cinema"
